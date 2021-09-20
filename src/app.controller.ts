20c0873742bb1933c82647839c88260af5895b6b
MythoAway7 <58316242+MythoAway7@users.noreply.github.com>
2021-09-19 23:03:58 -0500

    Formatting setup

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+    constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+    @Get()
+    getHello(): string {
+        return this.appService.getHello();
+    }
+
+    @Get('/board/:id')
+    getTest(@Param() params): string {
+        console.log(params.id);
+        return `${params.id} and more`;
+    }
 }
