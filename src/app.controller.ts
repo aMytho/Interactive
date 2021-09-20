f1675aac9512d83dd930f443561603538143de2a
MythoAway7 <58316242+MythoAway7@users.noreply.github.com>
2021-09-20 18:47:20 -0500

    Authentication

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Post, UseGuards, Request } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AuthService } from './auth/auth.service';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
-    constructor(private readonly appService: AppService) {}
+    constructor(
+        private readonly appService: AppService,
+        private readonly authService: AuthService) { }
 
     @Get()
     getHello(): string {
@@ -22,5 +27,18 @@ export class AppController {
         let boardInfo = `A board is the project hosting all of the info about your project. This contains the source code`
         let sessionInfo = `A session contains the board and the active users.`
         return [boardInfo, sessionInfo];
+    }
+
+    @UseGuards(LocalAuthGuard)
+    @Post('auth/login')
+    async login(@Request() req) {
+        console.log(req.user)
+        return this.authService.login(req.user);
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Get('profile')
+    getProfile(@Request() req) {
+        return req.user;
     }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BoardModule } from './board/board.module';
 import { SessionModule } from './session/session.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-    imports: [BoardModule, SessionModule],
+    imports: [BoardModule, SessionModule, AuthModule, UsersModule],
     controllers: [AppController],
     providers: [AppService],
 })
