63e88ca7a4c69b407ece8ef7b6380af541344aff
MythoAway <jshull04424@stu.usd313.org>
2021-09-30 15:42:05 +0000

    Client testing

--- static/example/scripts/main.js
+++ static/example/scripts/main.js
@@ -1,11 +1,33 @@
-let socket = io();
+let accessToken = "";
+let socket;
+let authInfo = {
+    transportOptions: {
+        polling: {
+          extraHeaders: {
+            Authorization: "Bearer " + accessToken, //'Bearer h93t4293t49jt34j9rferek...'
+          }
+        }
+      }
+}
 
-socket.on("msgToClient", (data) => {
-    console.log(data);
-    let log = document.createElement("li");
-    log.innerText = data;
-    document.getElementById("serverLogs").appendChild(log);
-})
+function connect() {
+    socket = io("/", authInfo);
+    socket.on("msgToClient", (data) => {
+        console.log(data);
+        let log = document.createElement("li");
+        log.innerText = data;
+        document.getElementById("serverLogs").appendChild(log);
+    })
+    socket.on("error", (data) => {
+        console.log(data);
+        alert(JSON.parse(data).toString())
+        let log = document.createElement("li");
+        log.innerText = JSON.parse(data);
+        document.getElementById("serverLogs").appendChild(log);
+    })
+}
+
+
 
 function makeRequest() {
     socket.emit("request", "echo");
@@ -25,4 +47,21 @@ function roomMSG() {
 
 function pipe() {
     socket.emit("findAllSession", {session: "test", user: "mytho", breed: "five", other: "string"})
+}
+
+function authRequest() {
+    socket.emit("auth", {lol: true})
+}
+
+async function auth() {
+    console.log("AUthing");
+    let request = await fetch("/auth/guest");
+    let response = await request.json();
+    alert("token is => " + response.access_token);
+    accessToken = response.accessToken;
+    connect();
+}
+
+function causeError() {
+    socket.emit("errorl", 5);
 }
